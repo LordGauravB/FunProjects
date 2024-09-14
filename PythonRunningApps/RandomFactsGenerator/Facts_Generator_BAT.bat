a974fb574a657e8bb5ba92ba1e648e7e98194b0d
gaurav3815 <gauravbhandari850@gmail.com>
2024-09-14 14:38:50 +0100

    Finalised

--- PythonRunningApps/RandomFactsGenerator/Facts_Generator_BAT.bat
+++ PythonRunningApps/RandomFactsGenerator/Facts_Generator_BAT.bat
@@ -1,2 +1,2 @@
 @echo off
-start pythonw "C:\Users\gaura\OneDrive\PC-Desktop\PythonRunningApps\RandomFactsGenerator\RandomFactsGenerator_Prod.py"
+start pythonw "C:\Users\gaura\OneDrive\PC-Desktop\PythonRunningApps\RandomFactsGenerator\[Python]RandomFactsGenerator_Prod.py"
